eec25b834284d9ab8acf04489ebf567830e88177
Marcos Churi <marcoschuri@gmail.com>
2021-04-19 00:15:38 -0300

    Mi primer commit

---
+++ SumaDeValoresAbsolutos.js
@@ -0,0 +1,6 @@
+var num1 = parseInt(prompt("Ingrese el primer valor"));
+var num2 = parseInt(prompt("Ingrese el segundo valor"));
+var num3 = parseInt(prompt("Ingrese el tercer valor"));
+
+var suma = Math.abs(num1) + Math.abs(num2) + Math.abs(num3);
+console.log ("La suma de los valores absolutos es: " + suma);
